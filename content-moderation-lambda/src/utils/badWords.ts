c3f3854c833cdfde99a433db2873a6b203de94e9
GabrielMorettii <gabrielmorettisilva@gmail.com>
2024-12-15 17:06:52 -0300

    refactor: remove bad words

--- content-moderation-lambda/src/utils/badWords.ts
+++ content-moderation-lambda/src/utils/badWords.ts
@@ -1,16 +1,1 @@
-export const badWords = [
-  "arrombado",
-  "babaca",
-  "caralho",
-  "cu",
-  "filho da puta",
-  "puta",
-  "foda",
-  "merda",
-  "piranha",
-  "porra",
-  "vagabunda",
-  "vadia",
-  "bosta",
-  "vai se fuder"
-];
+export const badWords = [];
